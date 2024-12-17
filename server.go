f2b4b981f6a2f81ed4f03ceb546d72f7376c6294
huazhimeng <2902151499@qq.com>
2024-12-17 21:37:58 +0800

    提交

--- server.go
+++ server.go
@@ -12,16 +12,18 @@ import (
 var server *http.Server
 var handles = make([]Handle, 0)
 
-func init() {
+func Listen() {
 	mux := http.NewServeMux()
 	//执行路由表
 	for _, handle := range handles {
 		register(mux, handle)
 	}
+
 	addr := ":90" + config.Value("MACHINE ID")
 	if addr == "" {
 		log.Fatalln("ADDR not found")
 	}
+
 	//启动服务
 	log.Printf("[http] listening %s\n", addr)
 	server = &http.Server{
